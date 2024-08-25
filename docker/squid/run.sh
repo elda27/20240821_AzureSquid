3e6c9df18a7f2dde66d5c279572576a5912cb3c4
elda27 <kaz.birdstick@gmail.com>
2024-08-26 00:30:21 +0900

    WIP

--- docker/squid/run.sh
+++ docker/squid/run.sh
@@ -7,6 +7,7 @@ docker run --rm \
   -e TZ=Asia/Tokyo\
   -v $SCRIPT_DIR/squid.conf:/etc/squid/squid.conf\
   -v $SCRIPT_DIR/allow_sites.txt:/etc/squid/allow_sites.txt\
+  -it\
   squid
   # -v $SCRIPT_DIR/secrets/squid.pem:/etc/squid/squid.pem\
   # -v $SCRIPT_DIR/secrets/squid.key:/etc/squid/squid.key\
